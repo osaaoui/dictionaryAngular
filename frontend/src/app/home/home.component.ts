59d4c5d14aaaf5527bd0ff8472c060861f40e969
omar saaoui <saaoui@gmail.com>
2019-01-19 16:59:16 -0500

    added parsing logic inside component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,7 +4,8 @@ import { Observable } from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import { map, filter } from '../../../node_modules/rxjs/operators';
 import {WebService } from '../web.service';
-import { ViewChild } from '@angular/core'
+import { ViewChild } from '@angular/core';
+import { Router } from "@angular/router";
 
 export interface Options{
   value: string;
@@ -26,14 +27,22 @@ export class HomeComponent implements OnInit {
   latinButton='';
   asuddimIsem= "";
   options: Options[] =[
-    {id: 1, value: "english-kabyle"},
-    {id: 2, value: "taqbaylit-taqnizit"}
+    {id: 1, value: "English-Kabyle"},
+    {id: 2, value: "Taqbaylit-Tagnizit"}
   ]
     
  selected = this.options[0].id;
-  
 
-  constructor(private webservice: WebService){}
+ untiTaqbaylit= ["tafunast", "tanemmirt","tameṭṭut", "tiγri","taṛumit", "tafat","tabburt", "tawwurt","timest", "tabbuct" ];
+ asgetTaqbaylit= ["idmaren", "idamen", "idammen"];
+
+
+ 
+ languageSelected(event: any){
+   this.selected= event;
+ }
+
+  constructor(private webservice: WebService, private router: Router){}
   ngOnInit() {
     
   }
@@ -45,12 +54,38 @@ export class HomeComponent implements OnInit {
     this.latin = event;
     this.latinButton += this.latin;
   }
-    //return this.webservice.getMessage().subscribe(words=> this.value = words);
+
+   tokenizeSpellingEnglish(item: string): string{
+    if((item[item.length -1]) =='s'){
+       item = item.substr(0, item.length -1);
+      }
+         return item;
+    
+  }
+  tokenizeSpellingKabyle(item: string): string{
+    if (item[0]=='t' && item[item.length - 1]== 't' && (this.untiTaqbaylit.includes(item)== false)){
+      item = item.slice(1, item.length - 1);
+  } else if (item[0]=='i' && item.slice(-2)== 'en' && (this.asgetTaqbaylit.includes(item)== false)){
+    item = 'a' + item.slice(1, item.length - 2);
+  } else if (item[0]=='t' && item[1]=='i' && item.slice(-2)== 'in'){
+    item = 'a' + item.slice(2, item.length - 2);
+    }
+         return item;
+    
+  }
+
     onNameKeyUp(event: any){
       //if(event.key === 'Enter'){
         this.spelling = event;
-        this.elem.nativeElement.focus(); 
+        this.spelling = this.spelling.toLowerCase();
+        if(this.selected ==1){
+          this.spelling = this.tokenizeSpellingEnglish(this.spelling);
+        }else{
+          this.spelling= this.tokenizeSpellingKabyle(this.spelling);
+        }
+        this.elem.nativeElement.focus();   
     }
+
 
   getWordList(){
     this.webservice.getWords(this.spelling, this.selected)
